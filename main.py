619887e8c88a7858d64c6d8c4fcb737ff56b2e55
h4sski <h4sski.programming.gmail.com>
2023-03-03 22:31:10 +0100

    push

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from tkinter import *
 from tkinter import ttk
+from map import Map
 
 from settings import Settings
 settings = Settings()
-print(settings.rows)
 
 window = Tk()
 window.title('Minesweeper')
@@ -11,17 +11,18 @@ window.title('Minesweeper')
 frame = ttk.Frame(window, padding=10)
 frame.grid()
 
-input_value = 'a'
+input_value = StringVar()
 ttk.Label(frame, text='Hello h4sski').grid(column=0, row=0)
 ttk.Button(frame, text='Quit', command=window.destroy).grid(column=0, row=1)
-ttk.Label(frame, textvariable=input_value).grid(column=0, row=2)
+ttk.Entry(frame, textvariable=input_value).grid(column=0, row=2)
 
 mine_frame = ttk.Frame(window, padding=10)
 mine_frame.grid()
 
-
-for i in range(settings.columns):
-    for j in range(settings.rows):
-        ttk.Button(mine_frame, text=i*j, width=3).grid(column=i, row=j)
+map = Map()
+map.generate_new_map()
+for i in range(settings.rows):
+    for j in range(settings.columns):
+        ttk.Button(mine_frame, text=map.mines[i][j], width=3).grid(row=i, column=j)
 
 window.mainloop()
--- settings.py
+++ settings.py
@@ -2,3 +2,4 @@ class Settings:
     def __init__(self):
         self.columns = 5
         self.rows = 5
+        self.mines_ratio = 0.2
